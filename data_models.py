f6d564e1ef67e20a9e8060a40c943221aa6285aa
unknown <magedfiverr@gmail.com>
2024-08-18 16:47:48 +0200

    add changes to Author and Books model

--- data_models.py
+++ data_models.py
@@ -3,12 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Author(db.Model):
+    __tablename__ = "authors"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     birth_date = db.Column(db.String)
     date_of_death = db.Column(db.String)
+    books = db.relationship('Book', backref='author', lazy=True)
 
 class Book(db.Model):
+    __tablename__ = "books"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     isbn = db.Column(db.Integer)
     title = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
